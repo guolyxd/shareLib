12cd2c4e4eb90adf136f6218984fe393afd9ccae
Antony <guolyxd@163.com>
2020-04-14 16:49:41 +0800

    test two agent node

--- sayHello.groovy
+++ sayHello.groovy
@@ -1,3 +1,21 @@
-def call(){
-    echo "Hello"
+pipeline{
+    agent none
+    stages{
+        stage('backend'){
+            agent{
+                docker{image 'maven:3-alpine'}
+            }
+            steps{
+                sh 'mvn --v'
+            }
+        }
+        stage('frontend'){
+            agent{
+                docker{image 'node:7-alpine'}
+            }
+            steps{
+                sh 'node -v'
+            }
+        }
+    }
 }
